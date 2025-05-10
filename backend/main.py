856adfc8d0e4906401add1bf794f2b07082ee7fb
mazenfkh <145606447+mazenfkh@users.noreply.github.com>
2025-05-11 00:53:41 +0300

    deployment issues

--- backend/main.py
+++ backend/main.py
@@ -16,9 +16,10 @@ from fastapi import (
 from pydantic import BaseModel, EmailStr
 from starlette.middleware.cors import CORSMiddleware
 
-from app.core.database import init_db
-from app.middleware.jwt_middleware import JWTMiddleware
-from app.mvc.controllers.analysis import analyze_risk, get_risk_report
+from backend.app.core.database import init_db
+from backend.app.middleware.jwt_middleware import JWTMiddleware
+from backend.app.mvc.controllers.analysis import analyze_risk, get_risk_report
+
 from app.mvc.controllers.rephrase import extract_full_text_from_stream
 from app.mvc.controllers.documents import (
     upload_file_to_gridfs,
