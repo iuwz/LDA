8c7b38864af6b2475bcce93bbef1ff88d2843b53
mazenfkh <mznfahad@gmail.com>
2025-05-14 09:13:23 +0300

    Update navbar.tsx

--- frontend/src/views/components/layout/navbar.tsx
+++ frontend/src/views/components/layout/navbar.tsx
@@ -212,7 +212,8 @@ const Navbar: React.FC = () => {
               <>
                 <Button
                   size="md"
-                  className={loginButtonStyle}
+                  variant="secondary"
+                  className="inline-flex items-center px-8 py-3 bg-gradient-to-r from-[#C17829] to-[#E3A063] text-white rounded-full font-semibold text-lg shadow-lg transition transform hover:scale-105"
                   onClick={handleLoginClick}
                 >
                   <div className="flex items-center space-x-1">
@@ -220,9 +221,11 @@ const Navbar: React.FC = () => {
                     <span>Login</span>
                   </div>
                 </Button>
+
                 <Button
                   size="md"
-                  className={registerButtonStyle}
+                  variant="primary"
+                  className="inline-flex items-center px-8 py-3 bg-gradient-to-r from-[#C17829] to-[#E3A063] text-white rounded-full font-semibold text-lg shadow-lg transition transform hover:scale-105"
                   onClick={handleRegisterClick}
                 >
                   Register
